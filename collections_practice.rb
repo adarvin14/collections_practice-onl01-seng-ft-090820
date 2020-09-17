9dbfd39a3db9370070c3d9504a4cdc8f9ca4084b
Alex Darvin <amdarvin14@gmail.com>
2020-09-17 23:56:58 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -8,6 +8,7 @@ def sort_array_desc(array)
 end
 
 
+<<<<<<< HEAD
 def sort_array_char_count(array)
   array.sort do |a, b|
     if a.length == b.length
@@ -18,40 +19,74 @@ def sort_array_char_count(array)
       -1
     end
   end
+=======
+
+
+def sort_array_char_count(array)
+    array.sort do |a, b|
+        if a.length == b.length
+          0
+        elsif a.length < b.length
+          -1
+        elsif a.length > b.length
+          1
+        end
+      end
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
 
 
 def swap_elements(array)
+<<<<<<< HEAD
   array[1], array[2] = array[2], array[1]
   array
 end
   
 def reverse_array(array)
     array.reverse
+=======
+  new_array = []
+end
+  
+def reverse_array(array)
+    new_array = []
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
 
 
 def kesha_maker(array)
   new_array = []
+<<<<<<< HEAD
   array.each do |i|
     i[2] = "$"
     new_array << i
   end
   new_array
+=======
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
 
 
 def find_a(array)
+<<<<<<< HEAD
   array.select {|word| word.start_with? "a"}
+=======
+  new_array = []
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
 
 
 def sum_array(array)
+<<<<<<< HEAD
   array.inject (:+)
+=======
+  
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
 
 
 def add_s(array)
+<<<<<<< HEAD
   array.each_with_index.collect do |word, index|
     if index == 1
       word = word
@@ -59,4 +94,7 @@ def add_s(array)
       word = word + "s"
     end  
   end  
+=======
+  
+>>>>>>> 95891861ccfadb840f14b6b46fd5bb664b409c78
 end
